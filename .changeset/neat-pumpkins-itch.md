e0ef55567b8d97bb640bdb3ed8ae19e515feca94
Subin Sebastian <subin.sebastian457@gmail.com>
2023-05-06 20:58:57 +0530

    divide

---
+++ .changeset/neat-pumpkins-itch.md
@@ -0,0 +1,5 @@
+---
+"my-package-publish-learning": patch
+---
+
+implemented divide method
--- index.ts
+++ index.ts
@@ -6,3 +6,7 @@ export const add = (a: number, b: number) => {
 export const subtract = (a: number, b: number) => {
   return a - b;
 };
+
+export const divide = (a: number, b: number) => {
+  return a / b;
+};
